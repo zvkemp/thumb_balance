2be3d61492f50943972a48955978c9582e3b8445
zvkemp <zvkemp@gmail.com>
2013-11-11 20:01:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThumbBalance::Application.config.secret_token = 'e899388a417dcd093a9316a16328e4d9054279b8cbfa7e79c6a0543102d6682d8675257e103afe8f7c09cc91a1bcb540a94ea35679d40680394321e773967dc8'
